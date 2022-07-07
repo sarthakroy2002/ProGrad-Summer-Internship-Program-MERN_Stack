b681fb81ca277cbe44392ae70bdd4b6e2c15bb6c
Sarthak Roy <sarthakroy2002@gmail.com>
2022-07-07 10:48:05 +0530

    Fixup Day 3 Task 2
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- Day 3/Day 3- Task 2/script.js
+++ Day 3/Day 3- Task 2/script.js
@@ -2,8 +2,8 @@ document.getElementById('player1').value = 0;
 document.getElementById('player2').value = 0;
 var p1 = 0;
 var p2 = 0;
-var result=30
-var count = 1
+var result= 30;
+var count = 1;
 
 function diceup()
 {
@@ -14,26 +14,29 @@ function diceup()
         p2 = p2 + randomnum;
         document.getElementById('player2').value = p2;
         count++;
-        printer();
     }
     else
     {
         p1 = p1 + randomnum;
         document.getElementById('player1').value = p1;
-        count++;
-        printer();
+        count++;    
     }
+    printer();
 }
 
 function printer()
 {        
-    if (result === p1)
+    if (result <= p1)
     {
         document.getElementById('result').innerText = 'Player 1 won the game';
+        alert('Player 1 won the game');
+        location.reload(true);
     }
-    else if (result === p2)
+    else if (result <= p2)
     {
         document.getElementById('result').innerText = 'Player 2 won the game';
+        alert('Player 2 won the game');
+        location.reload(true);
     }
     else
     {
